0ff0f9b63eb6234857e6e11fdfcbdef0e13deb9d
Muhamed Delic <muhamed.delicc@live.com>
2019-10-14 10:24:12 +1100

    Path of chrome executable now determined by OS type

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const puppeteer = require("puppeteer-core");
 const io = require("@actions/io");
 const os = require("os");
 
+function getChromePath() {
+  let browserPath = "";
+
+  if (os.type() === "Windows_NT") {
+    // Chrome is usually installed as a 32-bit application, on 64-bit systems it will have a different installation path.
+    const programFiles = os.arch() === 'x64' ? process.env["PROGRAMFILES(X86)"] : process.env.PROGRAMFILES;
+    browserPath = path.join(programFiles, "Google/Chrome/Application/chrome.exe");
+  } else if (os.type() === "Darwin") {
+    browserPath = "/Application/Google Chrome.app/Contents/MacOS/Google Chrome";
+  } else {
+    browserPath = "/usr/bin/google-chrome"; // Linux
+  }
+
+  return path.normalize(browserPath);
+}
+
 (async () => {
   await io.mkdirP(`${process.env.GITHUB_WORKSPACE}/screenshots/`);
-
-  console.log("OS Info --");
-  console.log(os.type());
-  console.log(os.release());
-  console.log("-- End OS Info")
 
   const url = core.getInput("url");
 
@@ -18,7 +29,7 @@ const os = require("os");
   const height = parseInt(core.getInput("height"));
 
   const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/google-chrome",
+    executablePath: getChromePath(),
     defaultViewport: { width, height }
   });
   const page = await browser.newPage();

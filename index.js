fdf0e260d147c0c5ed17772d9283481498eff150
Muhamed Delic <muhamed.delicc@live.com>
2019-10-14 13:40:37 +1100

    Refactor

--- index.js
+++ index.js
@@ -2,35 +2,33 @@ const core = require("@actions/core");
 const puppeteer = require("puppeteer-core");
 const io = require("@actions/io");
 
-async function runAction() {
-  await io.mkdirP(`${process.env.GITHUB_WORKSPACE}/screenshots/`);
+(async () => {
+  try {
+    await io.mkdirP(`${process.env.GITHUB_WORKSPACE}/screenshots/`);
 
-  const url = core.getInput("url");
+    const url = core.getInput("url");
 
-  const timestamp = new Date().getTime();
-  const width = parseInt(core.getInput("width"));
-  const height = parseInt(core.getInput("height"));
+    const timestamp = new Date().getTime();
+    const width = parseInt(core.getInput("width"));
+    const height = parseInt(core.getInput("height"));
 
-  const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/google-chrome",
-    defaultViewport: { width, height }
-  });
-  const page = await browser.newPage();
-  await page.goto(url, {
-    waitUntil: "networkidle2"
-  });
-  await page.waitFor(3000);
-  await page.screenshot({
-    fullPage: true,
-    path: `${process.env.GITHUB_WORKSPACE}/screenshots/screenshot-${timestamp}.png`
-  });
-  await browser.close();
+    const browser = await puppeteer.launch({
+      executablePath: "/usr/bin/google-chrome",
+      defaultViewport: { width, height }
+    });
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: "networkidle2"
+    });
+    await page.waitFor(3000);
+    await page.screenshot({
+      fullPage: true,
+      path: `${process.env.GITHUB_WORKSPACE}/screenshots/screenshot-${timestamp}.png`
+    });
+    await browser.close();
 
-  core.exportVariable("TIMESTAMP", timestamp);
-}
-
-try {
-  runAction();
-} catch (error) {
-  core.setFailed(`Failed to run action. ${error}`);
-}
+    core.exportVariable("TIMESTAMP", timestamp);
+  } catch (error) {
+    core.setFailed(`Failed to run action. ${error}`);
+  }
+})();

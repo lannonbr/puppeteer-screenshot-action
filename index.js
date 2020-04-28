9309e39b9796c8c5c1091659b3f469955208d1f7
Benjamin Lannon <benjamin@lannonbr.com>
2020-04-27 22:23:55 -0400

    Trying with MacOS

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ function getChromePath() {
 
   if (os.type() === "Windows_NT") {
     // Chrome is usually installed as a 32-bit application, on 64-bit systems it will have a different installation path.
-    const programFiles = os.arch() === 'x64' ? process.env["PROGRAMFILES(X86)"] : process.env.PROGRAMFILES;
-    browserPath = path.join(programFiles, "Google/Chrome/Application/chrome.exe");
+    const programFiles =
+      os.arch() === "x64"
+        ? process.env["PROGRAMFILES(X86)"]
+        : process.env.PROGRAMFILES;
+    browserPath = path.join(
+      programFiles,
+      "Google/Chrome/Application/chrome.exe"
+    );
   } else if (os.type() === "Linux") {
     browserPath = "/usr/bin/google-chrome";
+  } else if (os.type() === "Darwin") {
+    browserPath =
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
   }
 
   if (browserPath && browserPath.length > 0) {
@@ -31,20 +40,23 @@ function getChromePath() {
     const width = parseInt(core.getInput("width"));
     const height = parseInt(core.getInput("height"));
     const fullPage = core.getInput("fullPage") === "true";
-    const screenshotName = core.getInput("screenshotName") !== "false" ? core.getInput("screenshotName") : `screenshot-${timestamp}`;
+    const screenshotName =
+      core.getInput("screenshotName") !== "false"
+        ? core.getInput("screenshotName")
+        : `screenshot-${timestamp}`;
 
     const browser = await puppeteer.launch({
       executablePath: getChromePath(),
-      defaultViewport: { width, height }
+      defaultViewport: { width, height },
     });
     const page = await browser.newPage();
     await page.goto(url, {
-      waitUntil: "networkidle2"
+      waitUntil: "networkidle2",
     });
     await page.waitFor(3000);
     await page.screenshot({
       fullPage,
-      path: `${process.env.GITHUB_WORKSPACE}/screenshots/${screenshotName}.png`
+      path: `${process.env.GITHUB_WORKSPACE}/screenshots/${screenshotName}.png`,
     });
     await browser.close();
 
